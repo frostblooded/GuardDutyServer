8f34dfe612a64cc7ecd0ce9bcf8b38c0d17fafc7
frostblooded <frostblooded@yahoo.com>
2016-03-07 17:16:00 +0200

    scheduling runs tasks every minute

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module AttendanceCheckRailsapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.after_initialize do
+      scheduler = Rufus::Scheduler.new
+
+      scheduler.every '1m' do
+        puts "Printing: #{Time.current}"
+      end
+    end
   end
 end
