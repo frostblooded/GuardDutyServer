f9e78a6a574d531c50ef4b5b8c03babed93c1690
frostblooded <frostblooded@yahoo.com>
2016-03-07 16:43:41 +0200

    remove debug code

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,10 +1,9 @@
 class DevicesController < ApplicationController
-  # Doesn't expect authenticity token, so that POST request
+  # Doesn't expect authenticity token, so that POST requests
   # can be received from sources other than forms
   skip_before_action :verify_authenticity_token
 
   def create
-    puts params
     @device = Device.new(token: params[:token])
 
     if @device.save
