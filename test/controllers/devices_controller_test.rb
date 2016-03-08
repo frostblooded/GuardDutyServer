10f22a7087e1aa2c7c9601795911ab9e28f9a1d4
frostblooded <frostblooded@yahoo.com>
2016-03-08 12:52:55 +0200

    moved tests for model to correct file

--- test/controllers/devices_controller_test.rb
+++ test/controllers/devices_controller_test.rb
@@ -1,21 +1,4 @@
 require 'test_helper'
 
 class DevicesControllerTest < ActionController::TestCase
-  test "do not handle empty create params" do
-    assert_no_difference 'Device.count' do
-      post :create, {}
-    end
-  end
-
-  test "do not create device with token that has wrong length" do
-    assert_no_difference 'Device.count' do
-      post :create, token: 'a' * 100
-    end
-  end
-
-  test "create device with valid token length" do
-    assert_difference 'Device.count', 1 do
-      post :create, token: 'a' * 152
-    end
-  end
 end
--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @device = Device.new(token: 'a' * 152)
+  end
+
+  test "should be valid" do
+    assert @device.valid?
+  end
+
+  test "token should be present" do
+    @device.token = ''
+    assert_not @device.valid?
+  end
+
+  test "token should be of valid length" do
+    @device.token = 'a' * 100
+    assert_not @device.valid?
+  end
 end
