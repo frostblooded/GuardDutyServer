242c58cbedeff28491dcfb609c2fe67d7dbca048
frostblooded <frostblooded@yahoo.com>
2016-03-09 08:52:33 +0200

    changed minor details

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20160308195019) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password"
     t.string   "password_digest"
   end
 
