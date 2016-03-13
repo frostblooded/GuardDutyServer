ed2ff887dc77b043e306a5e356fcb092ea3d2ac3
frostblooded <frostblooded@yahoo.com>
2016-03-13 10:31:52 +0200

    trying to add last_name and first_name when signing up users and made tests work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :configure_permitted_parameters, :only => [:create]
 
 	def new
 		@user = User.new
@@ -13,4 +14,10 @@ class UsersController < ApplicationController
   	sign_out
   	redirect_to root_path
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    end
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,21 +1,20 @@
 require 'test_helper'
 
 class StaticPageControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "AttendanceCheck"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "AttendanceCheck"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "AttendanceCheck"
   end
 end
