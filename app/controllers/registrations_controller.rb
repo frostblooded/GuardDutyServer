d4b8c70bc33693247c13f5740f44ed11ce20c975
HristiyanZahariev <ihzahariev@gmail.com>
2016-03-14 16:52:05 +0200

    adding company name to the registration"

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:company_name, :username, :email, :password) }
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312213314) do
+ActiveRecord::Schema.define(version: 20160314144322) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "token"
@@ -32,8 +32,7 @@ ActiveRecord::Schema.define(version: 20160312213314) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "company_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
