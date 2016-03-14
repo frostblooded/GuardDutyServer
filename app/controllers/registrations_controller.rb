9d6985ceb09e0bdc430e6df293abe0b884b18bd1
frostblooded <frostblooded@yahoo.com>
2016-03-14 20:49:21 +0200

    changed minor devise settings

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,12 +3,12 @@ class RegistrationsController < Devise::RegistrationsController
 
 	protected
 
-		def configure_permitted_parameters
-			devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:company_name, :email, :password) }
-		  devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-    		user_params.permit(:company_name, :password)
-			end
-		end
-end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:company_name, :email, :password) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:company_name, :email, :password) }
+      devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:company_name, :password) }
 
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name,:last_name,:profile_name,:email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name,:last_name,:profile_name,:email, :password) }
+    end
 end
