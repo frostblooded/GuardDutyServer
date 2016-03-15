4b3c9b42a28a96e9f7d3a91130b764d5b7da154a
frostblooded <frostblooded@yahoo.com>
2016-03-15 09:31:01 +0200

    still doesn't work

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,6 +4,14 @@ class Company < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :company_name
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:company_name)
+    where(conditions).where(["lower(company_name) = :value", { :value => login.downcase }]).first
+  end
+
   def email_required?
     false
   end
