f3bf4866a56f9bf533a217c8b3adb48a6e6fdb0b
HristiyanZahariev <ihzahariev@gmail.com>
2016-03-27 18:36:51 +0300

    made very simple tests and tried to put some js

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,6 @@
 class Worker < ActiveRecord::Base
-  validates :first_name,	presence: true,	length: { maximum: 40}
-	validates :last_name,		presence: true,	length: { maximum: 40}
+  validates :first_name, presence: true,	length: { maximum: 40 }
+	validates :last_name,	presence: true,	length: { maximum: 40 }
 	has_secure_password
+	validates :password, presence: true, length: { minimum: 8 }
 end
--- test/models/worker_test.rb
+++ test/models/worker_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class WorkerTest < ActiveSupport::TestCase
   def setup
-    @worker = Worker.new(first_name: "Example FName", last_name: "Example LName")
+    @worker = Worker.new(first_name: "Example FName", last_name: "Example LName",
+    					 password: "Somethinglike", password_confirmation: "Somethinglike")
   end
 
   test "should be valid" do
@@ -27,5 +28,15 @@ class WorkerTest < ActiveSupport::TestCase
 	test "last name should not be too long" do
 		@worker.last_name = "a" * 50
 		assert_not @worker.valid?
-	end		
+	end
+
+	test "password should be present" do
+		@worker.password = @worker.password_confirmation = " " * 8
+		assert_not @worker.valid?
+	end
+
+	test "password should have a minimum length" do
+		@worker.password = @worker.password_confirmation = "a" * 7
+		assert_not @worker.valid?
+	end
 end
