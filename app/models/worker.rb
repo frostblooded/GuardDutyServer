3f6f5c2f7c6f5cf41d5d4ce64ac9b8e01550658c
frostblooded <frostblooded@yahoo.com>
2016-04-02 10:38:05 +0300

    Formatting code a bit

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,4 +1,4 @@
 class Worker < ActiveRecord::Base
   validates :first_name,	presence: true,	length: { maximum: 40}
- validates :last_name,		presence: true,	length: { maximum: 40}
+  validates :last_name,		presence: true,	length: { maximum: 40}
 end
