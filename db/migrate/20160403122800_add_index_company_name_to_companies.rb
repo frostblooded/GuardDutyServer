d690387057503b4e1481b94dc84dec9aa6530de6
HristiyanZahariev <ihzahariev@gmail.com>
2016-04-03 15:36:13 +0300

    Follow the styleguide

--- db/migrate/20160403122800_add_index_company_name_to_companies.rb
+++ db/migrate/20160403122800_add_index_company_name_to_companies.rb
@@ -1,5 +1,5 @@
 class AddIndexCompanyNameToCompanies < ActiveRecord::Migration
   def change
-  	add_index :companies, :company_name, unique: true
+		add_index :companies, :company_name, unique: true
   end
 end
