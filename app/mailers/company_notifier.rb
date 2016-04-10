2324ad6d6c2d1c7724e7c9666f29d6f0e9c7efc8
HristiyanZahariev <ihzahariev@gmail.com>
2016-04-11 00:59:16 +0300

    Mailer is still not working

--- app/mailers/company_notifier.rb
+++ app/mailers/company_notifier.rb
@@ -1,9 +1,9 @@
 class CompanyNotifier < ApplicationMailer
   default :from => 'ihzahariev@gmail.com'
 
-  def send_signup_email(company) #Just trying to see how sending an email works 
-    @company = company 
-    mail( :to => @company.email,
-      :subject => 'Thanks for signing up for our amazing app' )
+
+  def sample_email(company)
+    @company = company
+    mail(to: @company.email, subject: 'Sample Email')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,25 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => 'plain',
+    :user_name => "attendancecheck1337@gmail.com",
+    :password => "@zSumMnogoQk",
+    :enable_starttls_auto => true
+}
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
