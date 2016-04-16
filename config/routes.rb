76974c0d70ce1a421bd57e562cbb43a933662438
frostblooded <frostblooded@yahoo.com>
2016-04-16 19:32:17 +0300

    Fix device routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'workers'    => 'worker#create'
   get  'workers'    => 'worker#show'
   get  'settings'   => 'company_settings#show'
-  resources :devices
+  resources :device
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
