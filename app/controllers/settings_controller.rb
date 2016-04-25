c68969f4374dcd213ecff446969245b078cc454d
frostblooded <frostblooded@yahoo.com>
2016-04-25 11:05:33 +0300

    Seriously, Icak

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,7 +3,7 @@ class SettingsController < ApplicationController
     @daily_mail = params[:button_output]
     @company = current_company
 
-    if @daily_mail = "Yes"
+    if @daily_mail == "Yes"
       @company.settings(:daily_mail).send = "True"
     end
   end
