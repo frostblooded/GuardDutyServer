397def347312772048efb2a965a28842a9ac40dc
frostblooded <frostblooded@yahoo.com>
2016-04-26 11:21:12 +0300

    Modify some code

--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
   def setup
-    @device = Device.new(token: 'a' * 152)
+    @device = Device.new(gcm_token: 'a' * 152)
   end
 
   test "should be valid" do
     assert @device.valid?
   end
 
-  test "token should be present" do
-    @device.token = ''
+  test "GCM token should be present" do
+    @device.gcm_token = ''
     assert_not @device.valid?
   end
 
-  test "token should be of valid length" do
-    @device.token = 'a' * 100
+  test "GCM token should be of valid length" do
+    @device.gcm_token = 'a' * 100
     assert_not @device.valid?
   end
 end
