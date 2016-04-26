c945fb9fa9410675f1ecc7beee950c3704a9a9cb
frostblooded <frostblooded@yahoo.com>
2016-04-26 09:26:23 +0300

    Add test coverage

--- test/integration/api_test.rb
+++ test/integration/api_test.rb
@@ -2,28 +2,28 @@ require 'test_helper'
 
 class ApiTest < ActionDispatch::IntegrationTest
   def setup
-    @company = companies(:test_company)
+    @company = Company.create(company_name: 'test', password: 'foobarrr')
   end
 
   def request_access_token
     post '/api/v1/mobile/login', { company_name: @company.company_name,
-                                   password_digest: @company.encrypted_password }
+                                   password: @company.password }
     json = JSON.parse @response.body
     json['access_token']
   end
 
-  test 'Login return access token on success' do
+  test 'login return access token on success' do
     assert_not request_access_token.nil?
     assert_equal "201", @response.code
   end
 
-  test 'Forbid access to invalid token' do
+  test 'forbid access to invalid token' do
     access_token = request_access_token
     get '/api/v1/mobile/workers', { access_token: request_access_token + 'a' }
     assert_equal "401", @response.code
   end
 
-  test 'Get correct workers with access token' do
+  test 'GET correct workers with access token' do
     get '/api/v1/mobile/workers', { access_token: request_access_token }
     assert_equal "200", @response.code
     workers = JSON.parse @response.body
