69a2606ac3478447325abeec3b054d6f7cc04c04
frostblooded <frostblooded@yahoo.com>
2016-04-28 18:58:18 +0300

    Make worker names lowercase

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,7 +1,15 @@
 class Worker < ActiveRecord::Base
   belongs_to :company
+
   validates :first_name, presence: true,	length: { maximum: 40 }
   validates :last_name,	presence: true,	length: { maximum: 40 }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 8 }
+  has_secure_password
+
+  before_save :lowercase_names
+
+  def lowercase_names
+    self.first_name = self.first_name.downcase
+    self.last_name = self.last_name.downcase
+  end
 end
