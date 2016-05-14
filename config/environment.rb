d7289687a5f37aae49ee11338deb6809036a5be8
HristiyanZahariev <ihzahariev@gmail.com>
2016-05-15 01:27:33 +0300

    Still working on production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app49921759@heroku.com',
+  :password => '8TrSnysfTfp1',
+  :domain => 'hidden-shelf-43728.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
