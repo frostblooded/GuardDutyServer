ad9b8274ca0ba51fffd31a607602e79dc23ba9f0
HristiyanZahariev <ihzahariev@gmail.com>
2016-05-14 19:42:50 +0300

    Still working on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "gmail.com",
     :authentication => 'plain',
     :user_name => "attendancecheck1337@gmail.com",
     :password => "@zSumMnogoQk",
