ca1482d40f744bd995d819ec6c6edcd6fc780eaa
HristiyanZahariev <ihzahariev@gmail.com>
2016-05-15 01:30:20 +0300

    Still working on production

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
+  config.action_mailer.default_url_options = {:host => 'hidden-shelf-43728.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
   :user_name => 'app49921759@heroku.com',
   :password => '8TrSnysfTfp1',
   :domain => 'hidden-shelf-43728.herokuapp.com',
