fcd6176d2d97ddb101785af8b80c77bc2d997dff
frostblooded <frostblooded@yahoo.com>
2016-05-20 14:50:28 +0300

    Make temporary solution to calls only lasting a month

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,7 +1,9 @@
 class Call < ActiveRecord::Base
   belongs_to :worker
 
-  before_create :generate_token
+  before_create :generate_token, :check_worker_calls_count
+
+  DAYS_TO_KEEP = 30
 
   def valid_token?(passed_token)
     self.token == passed_token
@@ -21,5 +23,17 @@ class Call < ActiveRecord::Base
       begin
         self.token = SecureRandom.hex
       end while self.class.exists?(token: token)
+    end
+
+    # When a call is created, delete all calls
+    # which are older than they should be
+    # TODO: Move to a cronjob
+    def check_worker_calls_count
+      unless worker.nil? || worker.calls.count == 0
+        # A while instead of an if just in case
+        while worker.calls.first.created_at <= DAYS_TO_KEEP.days.ago
+          worker.calls.first.destroy
+        end
+      end
     end
 end
