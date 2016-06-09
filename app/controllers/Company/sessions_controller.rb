abed65a2fbad3708c60f629c5dfa6fc467ec7e3b
HristiyanZahariev <ihzahariev@gmail.com>
2016-06-09 09:43:29 +0300

    It was downcasing when sign up but didnt downcase when log in

--- app/controllers/Company/sessions_controller.rb
+++ app/controllers/Company/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Company::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
+  before_save :lowercase_name
 
+  def lowercase_name
+    self.company_name = self.company_name.downcase
+  end
   # GET /resource/sign_in
   # def new
   #   super
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  config.case_insensitive_keys = [:email, :company_name]
 
   config.mailer_sender = ENV["SENDGRID_USERNAME"]
 
