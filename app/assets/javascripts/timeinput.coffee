20f5ec7d368e0ea9acc72a2ea9c4be0a0045a338
Nikolay Danailov <frostblooded@yahoo.com>
2016-06-27 09:36:31 +0300

    Make input init javascript and fix it

--- app/assets/javascripts/timeinput.coffee
+++
@@ -1,2 +0,0 @@
-$(document).ready ->
-	$('.timepicker').timepicker({defaultTime: 'value'});
---
+++ app/assets/javascripts/timeinput.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('.timepicker').timepicker({defaultTime: 'value'});
+	alert('ivan');
+});
