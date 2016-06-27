2d0f98bd8623587a65d36f7847ffd7b21576bf14
Nikolay Danailov <frostblooded@yahoo.com>
2016-06-27 09:40:45 +0300

    Refactor site settings updating code

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -17,17 +17,12 @@ class SiteController < ApplicationController
 
   def update
     @site = Site.find(params[:id])
-    @interval = params[:call_interval]
-    @start = params[:shift_start]
-    @end = params[:shift_end ]
 
-    @site.settings(:call).interval = @interval
+    @site.settings(:call).interval = params[:call_interval]
     @site.settings(:call).save!
 
-    @site.settings(:shift).start = @start
-    @site.settings(:shift).save!
-
-    @site.settings(:shift).end = @end
+    @site.settings(:shift).start = params[:shift_start]
+    @site.settings(:shift).end = params[:shift_end ]
     @site.settings(:shift).save!
 
     flash[:success] = "Settings saved"
