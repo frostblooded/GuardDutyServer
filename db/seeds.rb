ed2b142f1c30775d5da15fa43fbef28e9d5b9d23
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-08 20:33:45 +0300

    Write some more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 company = Company.create name: 'frostblooded',
                          password: 'foobarrr',
                          password_confirmation: 'foobarrr',
                          email: Faker::Internet.free_email,
                          confirmed_at: Time.now
-site = company.sites.create name: 'test site'
 
-5.times do |i|
-  site.workers.create(name: Faker::Name.name, password: 'foobarrr')
+5.times do
+  site = company.sites.create name: Faker::GameOfThrones.city
+
+  5.times do
+    site.workers.create(name: Faker::GameOfThrones.character, password: 'foobarrr')
+  end
 end
