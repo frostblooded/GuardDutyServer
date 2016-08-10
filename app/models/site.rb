2e721ad1fc715edada50171b5e8b005018419630
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-10 20:42:01 +0300

    Implement shift object creation

--- app/models/site.rb
+++ app/models/site.rb
@@ -15,7 +15,37 @@ class Site < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 40 }
 
+  # Returns data about the last COMPLETED shift
   def get_last_shift
-    Shift.new
+    shift = Shift.new
+    shift.site = self
+    shift_times = get_last_shift_times
+
+    shift.start = shift_times[:start]
+    shift.end = shift_times[:end]
+    shift.activities = Activity.where('created_at >= :shift_start AND created_at <= :shift_end',
+      {shift_start: shift.start, shift_end: shift.end}).to_a
+
+    shift
   end
+
+  private
+    # Return start and end of last COMPLETED shift in a hash
+    # NOTE: This method assumes that there is only one shift per day
+    def get_last_shift_times
+      shift_times = Hash.new
+      shift_times[:end] = Time.parse settings(:shift).end
+
+      while shift_times[:end] > Time.now
+        shift_times[:end] -= 1.day
+      end
+
+      shift_times[:start] = Time.parse settings(:shift).start
+
+      while shift_times[:start] > shift_times[:end]
+        shift_times[:start] -= 1.day
+      end
+
+      shift_times
+    end
 end
--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -4,9 +4,74 @@ class SiteTest < ActiveSupport::TestCase
   def setup
     @company = create(:company)
     @site = @company.sites.first
+    @worker = @site.workers.first
+
+    base_time = Time.parse('11:00')
+    @activities = []
+
+    6.times do |i|
+      activity = @worker.activities.create(category: :call)
+
+      # Adds minutes to the time so that the first and the last
+      # activities are not in the shift's time
+      activity.created_at = base_time - 5.minutes + 15.minutes*i
+      activity.updated_at = activity.created_at
+      activity.save!
+
+      @activities << activity
+    end
+
+    @site.settings(:shift).start = '11:00'
+    @site.settings(:shift).end = '12:00'
   end
 
   test 'position belongs to correct company' do
     assert_equal @company, @site.company
+  end
+
+  test 'correctly returns last completed shift times when shift has just ended' do
+    time = Time.parse(@site.settings(:shift).end) + 30.minutes
+
+    Timecop.freeze(time) do
+      shift_times = @site.instance_eval{ get_last_shift_times }
+      assert_equal shift_times[:start], Time.parse(@site.settings(:shift).start)
+      assert_equal shift_times[:end], Time.parse(@site.settings(:shift).end)
+    end
+  end
+
+  test 'correctly returns last completed shift times when a new shift has started and not ended' do
+    time = Time.parse(@site.settings(:shift).start) + 30.minutes
+
+    Timecop.freeze(time) do
+      shift_times = @site.instance_eval{ get_last_shift_times }
+      assert_equal shift_times[:start], (Time.parse(@site.settings(:shift).start) - 1.day)
+      assert_equal shift_times[:end], (Time.parse(@site.settings(:shift).end) - 1.day)
+    end
+  end
+
+  test 'correctly returns last completed shift times when a new shift is about to start' do
+    time = Time.parse(@site.settings(:shift).start) - 30.minutes
+
+    Timecop.freeze(time) do
+      shift_times = @site.instance_eval{ get_last_shift_times }
+      assert_equal shift_times[:start], (Time.parse(@site.settings(:shift).start) - 1.day)
+      assert_equal shift_times[:end], (Time.parse(@site.settings(:shift).end) - 1.day)
+    end
+  end
+
+  test 'correctly returns last shift' do
+    shift = @site.get_last_shift
+
+    # Make sure it returns only the activities which are part of the shift
+    assert_not shift.activities.include?(@activities[0])
+    assert shift.activities.include?(@activities[1])
+    assert shift.activities.include?(@activities[2])
+    assert shift.activities.include?(@activities[3])
+    assert shift.activities.include?(@activities[4])
+    assert_not shift.activities.include?(@activities[5])
+
+    assert_equal Time.parse(@site.settings(:shift).start), shift.start
+    assert_equal Time.parse(@site.settings(:shift).end), shift.end
+    assert_equal @site, shift.site
   end
 end
