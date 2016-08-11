bbffd8e63a863dbc009d2ee56bcc86c0bc541f01
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-11 16:32:24 +0300

    Make constructors for ShiftReport and WorkerReport

--- lib/shift_report.rb
+++ lib/shift_report.rb
@@ -1,4 +1,9 @@
 class ShiftReport
   attr_accessor :site
   attr_accessor :worker_reports
+
+  def initialize(site = nil, worker_reports = [])
+    @site = site
+    @worker_reports = worker_reports
+  end
 end
--- lib/worker_report.rb
+++ lib/worker_report.rb
@@ -1,4 +1,9 @@
 class WorkerReport
   attr_accessor :worker
   attr_accessor :messages
+
+  def initialize(worker = nil, messages = [])
+    @worker = worker
+    @messages = messages
+  end
 end
