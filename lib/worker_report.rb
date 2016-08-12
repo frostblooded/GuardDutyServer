cff15ce36d07a9a4546bd76109de1cd811d1de1e
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-12 14:07:05 +0300

    Add shift reference in worker report

--- lib/worker_report.rb
+++ lib/worker_report.rb
@@ -1,9 +1,13 @@
 class WorkerReport
   attr_accessor :worker
+  attr_accessor :activities
+  attr_accessor :shift
   attr_accessor :messages
 
-  def initialize(worker = nil, messages = [])
+  def initialize(worker = nil, activities = [], shift = nil, messages = [])
     @worker = worker
+    @activities = activities
+    @shift = shift
     @messages = messages
   end
 end
--- test/libs/worker_report_test.rb
+++ test/libs/worker_report_test.rb
@@ -3,12 +3,14 @@ require 'test_helper'
 class WorkerReportTest < ActiveSupport::TestCase
   def setup
     @worker = create(:worker)
-    @worker_report = WorkerReport.new(@worker)
+    @shift = Shift.new
+    @worker_report = WorkerReport.new(@worker, [], @shift)
   end
 
   test 'worker report initializes correctly' do
     assert_equal @worker, @worker_report.worker
     assert @worker_report.activities.empty?
+    assert_equal @shift, @worker_report.shift
     assert @worker_report.messages.empty?
   end
 end
