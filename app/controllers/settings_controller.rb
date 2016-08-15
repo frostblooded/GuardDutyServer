f3886d39ab361b4055e1fd29fadf343a792d600f
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-15 19:32:00 +0300

    Refactor settings update code some more

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -10,14 +10,9 @@ class SettingsController < ApplicationController
   def update
     @company = current_company
     @company.settings(:mail).additional = params[:add_email]
-
-    if params[:daily_mail] == "true"
-      @company.settings(:mail).daily = "True"
-    else
-      @company.settings(:mail).daily = "False"
-    end
-
+    @company.settings(:mail).daily = params[:daily_mail] ? "True" : "False"
     @company.settings(:mail).save!
+    
     flash[:success] = "Settings saved"
     redirect_to settings_path
   end
