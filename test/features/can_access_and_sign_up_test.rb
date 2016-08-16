951b560f1a7ee53431ecb5e4c49426c4a3d5ce50
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-16 10:07:58 +0300

    Fix sign up acceptance test

--- test/features/can_access_and_sign_up_test.rb
+++ test/features/can_access_and_sign_up_test.rb
@@ -1,20 +1,20 @@
 require "test_helper"
 
 class CanAccessAndSignUpTest < Capybara::Rails::TestCase
-  test "canAccessSignUp" do
+  test "can access sign up" do
     visit new_company_registration_path
     assert_content page, 'Password confirmation (8 characters minimum)'
   end
 
- # test "canActuallyLogin" do
- #    visit new_company_session_path
+ test "can sign up" do
+    visit new_company_registration_path
 
- #    fill_in 'Name', with: 'SomeRandomNameKappa'
- #    fill_in 'Email', with: 'somerandommail@example.org'
- #    fill_in 'Password', with: 'foobarbarbar'
- #    fill_in 'Password confirmation', with: 'foobarbarbar'
- #    click_button 'Sign up'
+    fill_in 'company_name', with: 'SomeRandomNameKappa'
+    fill_in 'company_email', with: 'somerandommail@example.org'
+    fill_in 'company_password', with: 'foobarrr'
+    fill_in 'company_password_confirmation', with: 'foobarrr'
+    click_button 'Sign up'
 
- #    assert_content page,  'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
- #  end
+    assert_content page,  'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
+  end
 end
