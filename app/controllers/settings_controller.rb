c9c9e703f9e7a0548f155a1857b1c2820329cd8c
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-16 16:44:18 +0300

    Rename and refactor company settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,17 +1,12 @@
 class SettingsController < ApplicationController
   def index
-    if params[:daily_mail] == "true"
-      @daily_mail == true
-    end
-    
     @company = current_company
   end
 
   def update
     @company = current_company
-    @company.settings(:email).additional = params[:add_email]
-    @company.settings(:email).daily = params[:daily_mail] ? true : false
-    @company.settings(:email).time = params[:mail_time]
+    @company.settings(:email).wanted = params[:email_wanted] ? true : false
+    @company.settings(:email).time = params[:email_time]
     @company.settings(:email).save!
 
     flash[:success] = "Settings saved"
