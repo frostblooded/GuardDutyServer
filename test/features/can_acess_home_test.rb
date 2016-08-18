cdab541ef397d1677cb52491f1b925860fa96520
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-18 18:39:14 +0300

    Fix home acceptance test

--- test/features/can_acess_home_test.rb
+++ test/features/can_acess_home_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class CanAcessHomeTest < Capybara::Rails::TestCase
   test 'visit root' do
     visit root_path
-    assert_content page, 'AttendanceCheck'
+    assert_content page, 'Attendance Check'
   end
 
   test 'link contact' do
     visit root_path
     click_link 'Contact'
-    assert_content page, 'should insert'
+    assert_content page, 'nikolay_danailov@yahoo.com'
   end
 end
