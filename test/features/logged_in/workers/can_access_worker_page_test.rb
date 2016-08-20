82af62666fb09b0461d30143c52547d2335f8f45
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-20 18:38:32 +0300

    Write acceptance tests for worker page

---
+++ test/features/logged_in/workers/can_access_worker_page_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class CanAccessWorkerPageTest < Capybara::Rails::TestCase
+  def setup
+    @company = create(:company)
+    login_as @company
+    @worker = @company.workers.first
+
+    visit worker_path(@worker)
+  end
+
+  test 'displays correct data' do
+    assert_text @worker.name
+
+    within '#sites' do
+      @worker.sites.each do |s|
+        assert_text s.name
+      end
+    end
+
+    within '.wtable' do
+      @worker.activities.each do |a|
+        assert_text a.created_at.localtime
+      end
+    end
+  end
+end
