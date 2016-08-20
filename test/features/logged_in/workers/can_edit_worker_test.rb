c2eee3accc28ad5eba7d6a76e17ca04c45f4bcac
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-20 18:46:26 +0300

    Make worker edit acceptance tests

---
+++ test/features/logged_in/workers/can_edit_worker_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class CanEditWorkerTest < Capybara::Rails::TestCase
+  def setup
+    @company = create(:company)
+    login_as @company
+
+    @worker = @company.workers.first
+    @new_name = @worker.name + 'a'
+
+    visit edit_worker_path(@worker)
+  end
+
+  test 'editting worker works' do
+    new_password = 'foobarrra'
+
+    fill_in 'worker_name', with: @new_name
+    fill_in 'worker_password', with: new_password
+    fill_in 'worker_password_confirmation', with: new_password
+
+    click_button 'Save Changes'
+    assert_equal workers_path, current_path
+    assert_text 'Changes saved'
+  end
+
+  test 'worker edit shows error on nonmatching passwords' do
+    new_password = 'foobarrr'
+
+    fill_in 'worker_name', with: @new_name
+    fill_in 'worker_password', with: new_password
+    fill_in 'worker_password_confirmation', with: new_password + 'a'
+
+    click_button 'Save Changes'
+    assert_equal worker_path(@worker), current_path
+    assert_text 'Password confirmation doesn\'t match Password'
+  end
+end
