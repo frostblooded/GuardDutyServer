90dde33f3bf0bc31adc2531c167b84025796ed0a
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-20 20:08:39 +0300

    Add some more assertions to site page settings updating

--- test/features/logged_in/sites/can_access_site_page_test.rb
+++ test/features/logged_in/sites/can_access_site_page_test.rb
@@ -6,6 +6,10 @@ class CanAccessSitePageTest < Capybara::Rails::TestCase
     @site = @company.sites.first
     login_as @company
 
+    @new_call_interval = '20'
+    @new_shift_start = '20:00'
+    @new_shift_end = '21:00'
+
     visit site_path(@site)
   end
 
@@ -20,12 +24,17 @@ class CanAccessSitePageTest < Capybara::Rails::TestCase
   end
 
   test 'site settings get updated correctly' do
-    fill_in 'call_interval', with: '20'
-    fill_in 'shift_start', with: '20:00'
-    fill_in 'shift_end', with: '21:00'
+    fill_in 'call_interval', with: @new_call_interval
+    fill_in 'shift_start', with: @new_shift_start
+    fill_in 'shift_end', with: @new_shift_end
     click_button 'Save changes'
 
     assert_equal site_path(@site), current_path
     assert_text 'Settings saved'
+
+    @site.reload
+    assert_equal @new_call_interval, @site.settings(:call).interval
+    assert_equal @new_shift_start, @site.settings(:shift).start
+    assert_equal @new_shift_end, @site.settings(:shift).end
   end
 end
