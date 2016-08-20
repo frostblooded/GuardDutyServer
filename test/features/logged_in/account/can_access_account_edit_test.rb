52ac72f18acd7deb75f92493dcf09888db0721c2
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-20 20:05:20 +0300

    Start making account edit acceptance tests

---
+++ test/features/logged_in/account/can_access_account_edit_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+class CanAccessAccountEditTest < Capybara::Rails::TestCase
+  def setup
+    @current_password = 'foobarrr'
+
+    @company = create(:company)
+    @company.update password: @current_password
+
+    @new_email = @company.email + 'a'
+    @new_password = 'barrrfoo'
+
+    login_as @company
+    visit edit_company_registration_path
+  end
+
+  test 'shows correct default email' do
+    assert_equal @company.email, find('#company_email').value
+  end
+
+  test 'correctly updates settings' do
+    fill_in 'company_email', with: @new_email
+    fill_in 'company_password', with: @new_password
+    fill_in 'company_password_confirmation', with: @new_password
+    fill_in 'company_current_password', with: @current_password
+    click_button 'Update'
+
+    assert_equal root_path, current_path
+    assert_text 'Your account has been updated successfully'
+
+    @company.reload
+    assert_equal @new_email, @company.email
+    assert @company.valid_password?(@new_password)
+  end
+
+  test 'returns error on nonmatching passwords' do
+
+  end
+
+  test 'shows error on invalid current password' do
+    
+  end
+end
