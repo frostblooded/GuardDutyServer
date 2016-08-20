2b94ca933f6b5b66c19892984319552a8521aea0
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-20 17:18:42 +0300

    Small site creation test refactor

--- test/features/logged_in/sites/can_create_new_site_test.rb
+++ test/features/logged_in/sites/can_create_new_site_test.rb
@@ -3,13 +3,12 @@ require "test_helper"
 class CanCreateNewSiteTest < Capybara::Rails::TestCase
   def setup
     login_as create(:company)
-    @site = build(:site)
     visit new_site_path
   end
 
   test 'can create new site' do
     assert_difference 'Site.count' do
-      fill_in 'site_name', with: @site.name
+      fill_in 'site_name', with: 'test site'
       click_button 'Create new site'
     end
 
