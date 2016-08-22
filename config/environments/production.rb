c113a201e9499235d24b3bf2132d59534fa577d2
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-22 21:30:46 +0300

    Fix production log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
+  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
 
   config.action_mailer.default_url_options = { :host => '37.157.182.179', port: '3000'}
 
