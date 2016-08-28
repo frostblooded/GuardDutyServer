13cdcbe1c137caccb0ae02f7813ef9f3a7a84266
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-28 13:10:38 +0300

    Make time regex app wide

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module AttendanceCheckRailsapp
   class Application < Rails::Application
+    config.time_regex = /([01]?[0-9]|2[0-3]):[0-5][0-9]/
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
