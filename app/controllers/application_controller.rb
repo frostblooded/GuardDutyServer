5c97edd80510b90538e8a934ad1df352f5dcd45c
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-28 10:14:09 +0300

    Remove some code about old pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_company!, except: [:home, :help, :about,
-                                                 :contact]
+  before_action :authenticate_company!, except: [:home, :contact]
 
   protected
 
