e9798d2c86b7a43d6b5abc8a9b8e06a5b7ca338e
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-29 21:09:31 +0300

    Make site page workers forntend work

--- app/assets/javascripts/site/show.js
+++ app/assets/javascripts/site/show.js
@@ -1,7 +1,9 @@
+$(document).ready(create_new_worker_input);
+
 function create_worker_input(worker_name) {
-  $worker = $('<form class="worker"></form>');
-  $worker_input = $('<input class="worker-input" type="text" value="' + worker_name + '"/>');
-  $worker_remove = $('<input class="worker-remove" type="button" value="remove"/>');
+  var $worker = $('<div class="worker"></div>');
+  var $worker_input = $('<input class="worker-input" type="text" value="' + worker_name + '"/>');
+  var $worker_remove = $('<input class="worker-remove" type="button" value="remove"/>');
 
   $worker_remove.click(function() {
     $(this).parent().remove();
@@ -10,4 +12,22 @@ function create_worker_input(worker_name) {
   $worker.append($worker_input);
   $worker.append($worker_remove);
   $('.workers').append($worker);
+}
+
+function create_new_worker_input() {
+  var $new_worker = $('<div class="new-worker"></div>');
+  var $worker_input = $('<input class="worker-input" type="text"/>');
+  var $worker_add = $('<input class="worker-add" type="button" value="add"/>');
+
+  $worker_add.click(function() {
+    create_worker_input($worker_input.val());
+
+    // Create new new worker input
+    $(this).parent().remove();
+    create_new_worker_input();
+  });
+
+  $new_worker.append($worker_input);
+  $new_worker.append($worker_add);
+  $('.workers').append($new_worker);
 }
