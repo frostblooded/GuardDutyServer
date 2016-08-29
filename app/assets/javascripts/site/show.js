0fbc0d2a4a98c74ae70199833a6ac63e458d5a2c
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-29 20:33:13 +0300

    Add some style to worker list in site page

--- app/assets/javascripts/site/show.js
+++ app/assets/javascripts/site/show.js
@@ -1,7 +1,7 @@
 function create_worker_input(worker_name) {
   $worker = $('<form class="worker"></form>');
   $worker_input = $('<input class="worker-input" type="text" value="' + worker_name + '"/>');
-  $worker_remove = $('<input class="remove-worker" type="button" value="remove"/>');
+  $worker_remove = $('<input class="worker-remove" type="button" value="remove"/>');
 
   $worker_remove.click(function() {
     $(this).parent().remove();
