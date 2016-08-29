b9112f146d4c3b83c55953465e7e34b3418810a3
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-29 20:17:29 +0300

    Add workers to site page

---
+++ app/assets/javascripts/site/show.js
@@ -0,0 +1,13 @@
+function create_worker_input(worker_name) {
+  $worker = $('<form class="worker"></form>');
+  $worker_input = $('<input class="worker-input" type="text" value="' + worker_name + '"/>');
+  $worker_remove = $('<input class="remove-worker" type="button" value="remove"/>');
+
+  $worker_remove.click(function() {
+    $(this).parent().remove();
+  });
+
+  $worker.append($worker_input);
+  $worker.append($worker_remove);
+  $('.workers').append($worker);
+}
