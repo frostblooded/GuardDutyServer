9a072839a88fe775bbade7c759f975eb61dd2413
Nikolay Danailov <frostblooded@yahoo.com>
2016-08-30 08:50:33 +0300

    FINALLY FIXED TESTS!

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,6 +23,21 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 
+# Be able to use login_as without locking
+# the database
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+
+# Forces all threads to share the same connection. This works on
+# Capybara because it starts the web server in a thread.
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+
 module ActiveSupport
   class TestCase
     # Included so that you don't need to add the 'FactoryGirl.' prefix
