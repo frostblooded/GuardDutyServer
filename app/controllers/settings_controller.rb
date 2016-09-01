f5a82cd001f04d8cc360b30ac25bf482e84326b5
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-01 16:36:25 +0300

    Add email validations to recipient emails in settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,18 +5,19 @@ class SettingsController < ApplicationController
   end
 
   def update
+    @errors = []
     validate_email_time
+    save_recipients
 
-    if flash.empty?
-      @company = current_company
-      @company.settings(:email).recipients = params[:recipients]
-      @company.settings(:email).wanted = params[:email_wanted] ? true : false
-      @company.settings(:email).time = params[:email_time]
-      @company.settings(:email).save!
+    if @errors.empty?
+      current_company.settings(:email).wanted = params[:email_wanted] ? true : false
+      current_company.settings(:email).time = params[:email_time]
+      current_company.settings(:email).save!
 
       flash[:success] = 'Settings saved'
     end
 
+    flash[:danger] = @errors.join ', ' unless @errors.empty?
     redirect_to settings_path
   end
 
@@ -24,7 +25,21 @@ class SettingsController < ApplicationController
     time_regex = Rails.application.config.time_regex
 
     if !(params[:email_time] =~ time_regex)
-      flash[:danger] = 'Invalid email time format'
+      @errors << 'Invalid email time format'
     end
+  end
+
+  def save_recipients
+    current_company.settings(:email).recipients = []
+
+    params[:recipients].each do |r|
+      unless r =~ Rails.application.config.email_regex
+        @errors << "The email \"#{r}\" is invalid" 
+      else
+        current_company.settings(:email).recipients << r
+      end
+    end
+
+    current_company.save!
   end
 end
--- test/features/logged_in/settings/can_access_settings_test.rb
+++ test/features/logged_in/settings/can_access_settings_test.rb
@@ -12,6 +12,7 @@ class CanAccessSettingsTest < Capybara::Rails::TestCase
 
     @company.settings(:email).recipients = ['ivan@example.com', 'petkan@example.com']
     @added_email = 'frostblooded@example.com'
+    @invalid_email = 'rgergreg@wefwef.'
 
     login_as @company
     
@@ -70,5 +71,21 @@ class CanAccessSettingsTest < Capybara::Rails::TestCase
 
     @company.reload
     assert_not @company.settings(:email).recipients.include? @email
+  end
+
+  test 'adding invalid email shows error but saves valid ones' do
+    within '.new-email' do
+      find('.new-email-input').set @invalid_email
+      find('.new-email-add').click
+      find('.new-email-input').set @added_email
+      find('.new-email-add').click
+    end
+
+    click_button 'Save changes'
+    assert_text "The email \"#{@invalid_email}\" is invalid"
+
+    @company.reload
+    assert_not @company.settings(:email).recipients.include? @invalid_email
+    assert @company.settings(:email).recipients.include? @added_email
   end
 end
