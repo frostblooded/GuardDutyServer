dee869ee5bd45ffa32ee746e200434589011363c
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-10 17:16:52 +0300

    Prepare migration for mysql

--- db/migrate/20160511060448_change_company_and_worker_references.rb
+++ db/migrate/20160511060448_change_company_and_worker_references.rb
@@ -1,6 +1,7 @@
 class ChangeCompanyAndWorkerReferences < ActiveRecord::Migration
   def change
     add_reference :workers, :site, index: true, foreign_key: true
+    remove_index :workers, :company
     remove_reference :workers, :company
   end
 end
