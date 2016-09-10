a078bb8a69fe99bfa669967839cd550299dc7d26
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-10 17:48:20 +0300

    Add mysql and use correct id name

--- db/migrate/20160511060448_change_company_and_worker_references.rb
+++ db/migrate/20160511060448_change_company_and_worker_references.rb
@@ -1,7 +1,7 @@
 class ChangeCompanyAndWorkerReferences < ActiveRecord::Migration
   def change
     add_reference :workers, :site, index: true, foreign_key: true
-    remove_index :workers, name: :company
+    remove_index :workers, name: :company_id
     remove_reference :workers, :company
   end
 end
