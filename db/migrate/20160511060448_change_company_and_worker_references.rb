33b55e271b2799ae15e7cff915c8e23f8129924e
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-10 17:37:06 +0300

    What if I remove index like that

--- db/migrate/20160511060448_change_company_and_worker_references.rb
+++ db/migrate/20160511060448_change_company_and_worker_references.rb
@@ -1,6 +1,6 @@
 class ChangeCompanyAndWorkerReferences < ActiveRecord::Migration
   def change
     add_reference :workers, :site, index: true, foreign_key: true
-    remove_reference :workers, :company
+    remove_reference :workers, :company, index: true
   end
 end
