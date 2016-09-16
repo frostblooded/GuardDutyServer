85c5faf9628e6d23c2244f7e32de63a8b97cc6f2
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-16 18:54:10 +0300

    Make locales able to be switched

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_company!, except: [:home, :contact]
 
+  before_action :set_locale
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:danger] = exception.message
     redirect_to root_path
@@ -29,5 +31,15 @@ class ApplicationController < ActionController::Base
                                              :password,
                                              :password_confirmation,
                                              :current_password])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+  # Override the default url options to allow locale settings
+  # in the url
+  def default_url_options(options = {})
+    { locale: I18n.locale }
   end
 end
