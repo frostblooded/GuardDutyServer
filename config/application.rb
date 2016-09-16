e2c648caa061450cb5e1774b4ad3d0b16d0d55b6
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-16 20:34:29 +0300

    Use i18n gem for errors and setup available locales

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module AttendanceCheckRailsapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.available_locales = [:en, :bg]
 
     # Adding 'api' folder to application files for the API
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
