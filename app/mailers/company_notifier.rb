40f931f17aa432ead474dd572e14a90a2608a63b
Nikolay Danailov <frostblooded@yahoo.com>
2016-09-24 19:23:06 +0300

    Translate worker messages in report too

--- app/mailers/company_notifier.rb
+++ app/mailers/company_notifier.rb
@@ -1,14 +1,14 @@
 # Manages the company notifier emails' actions
 class CompanyNotifier < ApplicationMailer
   def report_email(company, email = company.email)
-    @company = company
-
-    # The reports are stored in a variable, so that a new report
-    #  isn't made every time the report needs to be accessed
-    #  (through the last_shift methods of Site)
-    @shift_reports = @company.sites.map { |site| site.last_shift.report }
-
     I18n.with_locale(company.report_locale) do
+      @company = company
+
+      # The reports are stored in a variable, so that a new report
+      #  isn't made every time the report needs to be accessed
+      #  (through the last_shift methods of Site)
+      @shift_reports = @company.sites.map { |site| site.last_shift.report }
+
       mail to: email, subject: t('report.subject')
     end
   end
