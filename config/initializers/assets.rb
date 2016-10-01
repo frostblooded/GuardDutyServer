a12828e258c98bb1af174f3167846a750dfdde14
Antonio <amindov@abv.bg>
2016-10-01 17:46:18 +0000

    Sign buttons pretified

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( *.js site/show.css settings/index.css hover/hover-min.css company_notifier/report_email.css)
+Rails.application.config.assets.precompile += %w( *.js site/show.css settings/index.css hover/hover-min.css company_notifier/report_email.css static-pages/home.css)
